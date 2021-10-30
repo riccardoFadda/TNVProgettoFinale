7952280a297929fec794a386687c80800f7297f8
Giancarlo <gianmart83@gmail.com>
2021-10-30 13:19:47 +0200

    modifiche moviesFromDb component

--- Frontend/src/app/routes/moviesFromDB/moviesFromDB.component.ts
+++ Frontend/src/app/routes/moviesFromDB/moviesFromDB.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesFromDbService }from'../../services/movieFromDb.service';
+import { DataService } from '../../services/data.service';
+import { MovieData } from '../../models/data.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { GenresResultsInterface } from 'src/app/models/movieFromApi.model';
+import { UserInterface } from 'src/app/models/user.model';
+import { MoviesFromApiService } from 'src/app/services/movieFromApi.service';
 
 @Component({
   selector: 'app-moviesFromDB',
@@ -28,29 +34,121 @@ export class MoviesFromDBComponent implements OnInit {
     "movieCrew": []
   }
 
-  constructor(private dbService: MoviesFromDbService) { }
+  movieToDb1: MovieData = {
+    "addedBy" : 0,
+    "cast": "",
+    "counter": 0,
+    "director": "",
+    "evaluation": 0,
+    "genre": "",
+    "name": "",
+    "rated": false,
+    "reviews": "",
+    "apiId": 0
+  };
+  movieToDb2: MovieData = {
+    "addedBy" : 0,
+    "cast": "",
+    "counter": 0,
+    "director": "",
+    "evaluation": 0,
+    "genre": "",
+    "name": "",
+    "rated": false,
+    "reviews": "",
+    "apiId": 0
+  };
+  genres: GenresResultsInterface[];
+
+  currentUser: UserInterface;
+
+  constructor(private dbService: MoviesFromDbService, private dataService:DataService,private auth: AuthService, private movieService: MoviesFromApiService) { }
 
    ngOnInit(): void {
-    this.getMoviesDb();
-
+    this.auth.user.subscribe(
+      response => {
+        this.currentUser = response;
+        this.movieService.getGenres().subscribe(
+          response => {
+            this.genres=response.genres;
+            this.getMoviesDb();
+          }
+        )
+      },
+      error => console.log(error)
+    )
+    
   }
 
   getMoviesDb(){
-    this.getRandomMovie(this.randomMovie1);
-    this.getRandomMovie(this.randomMovie2);
+    this.getRandomMovie(this.randomMovie1, this.movieToDb1.apiId);
+    this.getRandomMovie(this.randomMovie2, this.movieToDb2.apiId);
   }
  
 
-  getRandomMovie(randomMovie){
+  getRandomMovie(randomMovie,movieToDb){
     this.dbService.getMovies().subscribe(
       response => {
         console.log(response);
-        randomMovie.movieData = response[this.dbService.getRandomInt(0,19)];
+        randomMovie.movieData = response[this.dbService.getRandomInt(0,30)];
         console.log(randomMovie.movieData);
     
-          },         
-        
-        )    
+      },                     
+   )    
   }
-  
+
+  addMovieToDb(randomMovie, movieToDb){
+    var alreadyInDb = false;
+    this.dbService.getMovies().subscribe(
+      response => {
+        response.forEach(element => {
+          if(element.apiId===randomMovie.movieData.id){
+            alreadyInDb=true;
+          }
+        });
+        if(alreadyInDb===false){
+          this.dataService.addEntry(movieToDb).subscribe(
+            response => console.log(response),
+            error => console.log(error)
+          )
+        }
+      },
+      error => console.log(error)
+    )
+  }
+
+
+  pickMovie(movie, movieToDb){
+    this.addMovieToDb(this.randomMovie1, this.movieToDb1);
+    this.addMovieToDb(this.randomMovie2, this.movieToDb2);
+    var movieId;
+    var newCounter;
+    this.dataService.getData().subscribe(
+      response => {
+        response.forEach(element => {
+          if(element.name===movieToDb.name && element.director===movieToDb.director) movieId=element.id;
+        });
+        this.dataService.getEntry(movieId).subscribe(
+          response => {
+            newCounter = response.counter;
+            newCounter++;
+            movieToDb.counter=newCounter;
+            movieToDb.id=movieId;
+            //console.log("modificato da mandare al db: ", movieToDb)
+            this.dataService.editEntry(movieToDb).subscribe(
+              response => {
+                console.log(response);
+                if(movie===this.randomMovie1) this.getRandomMovie(this.randomMovie2, this.movieToDb2);
+                else this.getRandomMovie(this.randomMovie1, this.movieToDb1);
+              },
+              error => console.log(error)
+            )
+          },
+          error => console.log(error)
+        )
+      },
+      error => console.log(error)
+    )
+  }
 }
+
