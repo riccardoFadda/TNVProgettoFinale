6c288b323ab9e2d27b924bcab15ac9e4ae22ccf3
Riccardo <rickr91@gmail.com>
2021-10-31 22:31:43 +0100

    Piccole correzioni

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import { LoginPageComponent } from './routes/login-page/login-page.component';
 import { FilterByGenreComponent } from './routes/filter-by-genre/filter-by-genre.component';
 import { WelcomePageComponent } from './routes/welcome-page/welcome-page.component';
 import { SortByDateComponent } from './routes/sort-by-date/sort-by-date.component';
-import { MoviesApiComponent } from './routes/movies-api/movies-api.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { MatchMoviesComponent } from './components/matchMovies/matchMovies.component';
@@ -27,7 +26,6 @@ const routes: Routes = [
   { path: "login", component: LoginPageComponent},
   { path: "filterByGenre", component: FilterByGenreComponent, canActivate: [AuthGuardService]},
   { path: "sortByDate", component:SortByDateComponent, canActivate: [AuthGuardService]},
-  { path: "moviesApi", component: MoviesApiComponent, canActivate: [AuthGuardService]},
   { path: "reviews", component: ReviewsComponent, canActivate: [AuthGuardService]},
   { path: "matchMovies", component: MatchMoviesComponent, canActivate: [AuthGuardService]},
   { path: "sign-up", component: SignUpComponent},
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -19,12 +19,10 @@ import { FilterByGenreComponent } from './routes/filter-by-genre/filter-by-genre
 import { GenrePipePipe } from './pipes/genrePipe/genre-pipe.pipe';
 import { WelcomePageComponent } from './routes/welcome-page/welcome-page.component';
 import { SortByDateComponent } from './routes/sort-by-date/sort-by-date.component';
-import { MoviesApiComponent } from './routes/movies-api/movies-api.component';
 import { AuthService } from './services/auth.service';
 import { UserService } from './services/user.service';
 import { MovieratingsService } from './services/movieratings.service';
 import { CommentsService } from './services/comments.service';
-import { MoviesApiService } from './services/moviesapi.service';
 import { ReviewsComponent } from './components/reviews/reviews.component';
 import { AuthGuardService } from './services/auth-guard.service';
 import { MatchMoviesComponent } from './components/matchMovies/matchMovies.component';
@@ -51,7 +49,6 @@ import { PopularComponent } from './routes/popular/popular.component';
     GenrePipePipe,
     WelcomePageComponent,
     SortByDateComponent,
-    MoviesApiComponent,
     ReviewsComponent,
     MatchMoviesComponent,
     MoviesFromDBComponent,
@@ -68,7 +65,7 @@ import { PopularComponent } from './routes/popular/popular.component';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [DataService, AuthService, UserService, MovieratingsService, CommentsService, MoviesApiService,MoviesFromApiService, AuthGuardService],
+  providers: [DataService, AuthService, UserService, MovieratingsService, CommentsService,MoviesFromApiService, AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
