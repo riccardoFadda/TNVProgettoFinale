93a6d206f5e84f752cce495396d2abb1a581fca9
Riccardo <rickr91@gmail.com>
2021-10-31 15:52:13 +0100

    Aggiunto il campo AddedBy ai details dei movies

--- Frontend/src/app/routes/details/details.component.ts
+++ Frontend/src/app/routes/details/details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from 'src/app/services/data.service';
 import { MovieData } from 'src/app/models/data.model';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-details',
@@ -10,23 +11,28 @@ import { MovieData } from 'src/app/models/data.model';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private dataService: DataService, 
-    private router : Router) { }
+  constructor(private route: ActivatedRoute, private dataService: DataService,
+    private router : Router, private usersService: UserService) { }
 
   dataEntry: MovieData;
   id: number;
   ratedOption : string;
+  user: string;
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-    this.fetchEntry()
-    
+    this.fetchEntry();
+
   }
 
   fetchEntry(){
     this.dataService.getEntry(this.id).subscribe( (res: any ) => {
       this.dataEntry = res;
       this.ratedOption= this.dataEntry.rated === true ? 'yes' : 'no';
+      this.usersService.getUserById(this.dataEntry.addedBy).subscribe(
+        response => this.user=response.username,
+        error => console.log(error)
+      )
     })
   }
 
